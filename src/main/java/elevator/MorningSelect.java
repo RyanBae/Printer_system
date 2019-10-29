c748f031a1edf15bd09ec1a380d872064be8edbc
ryan <ryan.bae@triplllet.com>
2019-10-29 20:23:41 +0900

    2019.10.29 factory pattern

---
+++ src/main/java/elevator/MorningSelect.java
@@ -0,0 +1,20 @@
+package elevator;
+
+import java.util.Random;
+
+public class MorningSelect implements SelectElevator{
+
+    @Override
+    public int algorithm() {
+        Random r = new Random();
+
+        int e = 0;
+        for(int i = 0; i<10; i++){
+            e = r.nextInt(3);
+            if(e % 2 != 0){
+                break;
+            }
+        }
+        return e;
+    }
+}
